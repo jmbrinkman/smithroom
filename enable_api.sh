5bca9d2b511631fb37bf48f7b3accf9c45dc8b4e
jmbrinkman <jmbrinkman@outlook.com>
2025-07-31 12:20:22 +0200

    Create enable_api.sh

---
+++ enable_api.sh
@@ -0,0 +1,7 @@
+gcloud services enable \
+aiplatform.googleapis.com \
+discoveryengine.googleapis.com \
+bigquery.googleapis.com \
+storage.googleapis.com \
+iam.googleapis.com \
+notebooks.googleapis.com
